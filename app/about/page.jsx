af61f1508dd19105bc5d15fb280fd3c0ed0fd1c5
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2024-05-15 14:23:10 +0200

    added aboutme

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
   return (
     <section className="p-2 flex flex-col gap-5 mt-5 justify-center items-center text-cyan-100">
       <h1 className="text-center text-2xl md:text-3xl underline decoration-wavy decoration-cyan-300 text-cyan-100">About me</h1>
-      <p className="text-xl md:text-3xl text-balance text-center">I am a junior Full-Stack devloper, although, I am inclined towards Front-end as a visual person but nevertheless, I am passionate about bringing ideas to life and experimenting with new technologies for better understanding. I thrive on creating seamless, user-friendly experience. Let’s collaborate and build something extraordinary together.</p>
+      <p className="text-xl md:text-3xl text-balance text-center">I am a Full-Stack devloper, although, I am inclined towards Front-end as a visual person, I am passionate about bringing ideas to life and experimenting with new technologies for better understanding. I thrive on creating seamless, user-friendly experience. I am not afraid to learn new approachs and concepts. Currently I am working with NextJS stack.  </p>
       <article className="w-full">
         <h2 className="text-center text-2xl md:text-3xl underline decoration-wavy decoration-cyan-300 text-cyan-100">Hobbies</h2>
         <ul className="text-2xl pl-0 sm:pl-10">
